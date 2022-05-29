77985edc8527bac090cbccbea9428865bd9ce1a3
obleus <odney@Odneys-MacBook-Air.local>
2022-05-29 16:51:18 -0400

    connecect signup route to the home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -78,6 +78,7 @@ router.get('/post/:id', (req, res) => {
 
             res.render('single-post', {
                 post,
+                
                 loggedIn: req.session.loggedIn
             });
         })
@@ -94,6 +95,15 @@ router.get('/login', (req, res) => {
     }
 
     res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    if (!req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('signup');
 });
 
 module.exports = router;
